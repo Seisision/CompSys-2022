3269e967c6fa8b8d0fe45ac8850a17f6413ac78f
ulriku <ulrikhlp@gmail.com>
2022-11-16 22:53:32 +0100

    Client can now recieve files

--- A3/src/networking.c
+++ A3/src/networking.c
@@ -12,6 +12,7 @@
 #endif
 
 #include "./networking.h"
+#include <netinet/in.h>
 #include "./sha256.h"
 
 char server_ip[IP_LEN];
@@ -32,14 +33,7 @@ struct response {
 };
 
 unsigned int bytes_to_int(char* bytes) {
-    unsigned int n = 0;
-
-    n = (unsigned int)bytes[3];
-    n = n | (((unsigned int)bytes[2]) << 8);
-    n = n | (((unsigned int)bytes[1]) << 16);
-    n = n | (((unsigned int)bytes[0]) << 24);
-
-    return n;
+    return htonl(*((unsigned int*)bytes));
 }
 
 void response_init(struct response *p, char *byte_reponse) {
@@ -60,6 +54,21 @@ void response_init(struct response *p, char *byte_reponse) {
     } else {
       p->message_body = NULL;
     }
+}
+
+void response_init_h(struct response *p, char *byte_reponse) {
+    p->block_hash = malloc(32);
+    p->total_hash = malloc(32);
+
+    p->data_length = bytes_to_int(byte_reponse);
+    p->status_code = bytes_to_int(byte_reponse+4);
+    p->block_number = bytes_to_int(byte_reponse+8);
+    p->block_count = bytes_to_int(byte_reponse+12);
+
+    memcpy(p->block_hash, byte_reponse+16, 32);
+    memcpy(p->total_hash, byte_reponse+48, 32);
+
+    p->message_body = NULL;
 }
 
 void cleanup_response(struct response *p) {
@@ -153,11 +162,14 @@ char* build_message(char* username, char* signature, char* msg, unsigned int msg
     memcpy(msg_data+49,msg_length_p+2,1);
     memcpy(msg_data+50,msg_length_p+1,1);
     memcpy(msg_data+51,msg_length_p,1);
+    printf("length in byte array: %d%d%d%d\n", msg_data[48], msg_data[49], msg_data[50], msg_data[51]);
 
     if (msg_length > 0) {
         memcpy(msg_data+52, msg, msg_length);
+        printf("passed message length: %d\n", msg_length);
+        printf("message: %s\n", msg);
+        printf("actual message length: %d\n", strlen(msg));
     }
-
     return msg_data;
 }
 
@@ -177,12 +189,11 @@ void register_user(char* username, char* password, char* salt)
     Rio_writen(server, data, 52);
     Rio_readinitb(&rio, server);
 
-    char buffer[MAXLINE];
+    char buffer[MAX_MSG_LEN];
     struct response r;
-    Rio_readlineb(&rio, buffer, MAXLINE);
+    Rio_readn(server, buffer, MAX_MSG_LEN);
 
     response_init(&r, buffer);
-    printf("status response: %d\n", r.status_code);
     printf("feedback: %s\n", r.message_body);
 
     cleanup_response(&r);
@@ -199,6 +210,62 @@ void get_file(char* username, char* password, char* salt, char* to_get)
 {
     // Your code here. This function has been added as a guide, but feel free 
     // to add more, or work in other parts of the code
+    hashdata_t signature;
+    get_signature(password, salt, signature);
+
+    rio_t rio;
+    int server = Open_clientfd("127.0.0.1", "23457");
+
+    char* data = build_message(username, signature, to_get, strlen(to_get));
+    Rio_writen(server, data, 52+strlen(to_get));
+    Rio_readinitb(&rio, server);
+
+    char buffer[80+MAX_MSG_LEN];
+    struct response* pr;
+    printf("fetching response\n");
+    size_t read_bytes = 0;
+    int counter = 0;
+
+    size_t total_message_length = 0;
+
+    struct response** response_buffer;
+    while (1)
+    {
+      if (Rio_readn(server, buffer, 80) != 80) break;
+      pr = malloc(sizeof(struct response));
+      response_init_h(pr, buffer);
+      if (pr->data_length > 0) {
+          pr->message_body = malloc(pr->data_length);
+          Rio_readn(server, pr->message_body, pr->data_length);
+      }
+      unsigned int block_number = pr->block_number;
+      unsigned int block_count = pr->block_count;
+
+      if (counter == 0) {
+        response_buffer = malloc(block_count*sizeof(struct response*));
+      }
+      response_buffer[block_number] = pr;
+      printf("buffer respponse %u\n", response_buffer[counter]->block_number);
+      counter++;
+      total_message_length += pr->data_length;
+      if(counter > block_count) break;
+    }
+
+    char* combined_message = malloc(total_message_length+1);
+    size_t progress = 0;
+
+    for (int i = 0; i < counter; ++i) {
+      memcpy(combined_message+progress,response_buffer[i]->message_body, response_buffer[i]->data_length);
+      progress += response_buffer[i]->data_length;
+      cleanup_response(response_buffer[i]);
+      free(response_buffer[i]);
+    }
+    free(response_buffer);
+
+    combined_message[total_message_length] = 0;
+    printf("%s", combined_message);
+
+    Close(server);
 }
 
 int main(int argc, char **argv)
@@ -295,8 +362,16 @@ int main(int argc, char **argv)
     // Register the given user
     register_user(username, password, user_salt);
 
+    printf("\n");
+    printf("------------------------NEW CALL get file tiny-------------------");
+    printf("\n");
+
     // Retrieve the smaller file, that doesn't not require support for blocks
     get_file(username, password, user_salt, "tiny.txt");
+
+    printf("\n");
+    printf("------------------------NEW CALL get file big-------------------");
+    printf("\n");
 
     // Retrieve the larger file, that requires support for blocked messages
     get_file(username, password, user_salt, "hamlet.txt");
